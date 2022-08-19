5d1f38148e03f9f12417f60dae6175aea25e1d52
Jakob <jakob.hjortshoj@noaignite.com>
2022-08-19 09:04:05 +0200

    Project setup done

---
+++ JP Apex Predators/JP Apex Predators/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JP Apex Predators
+//
+//  Created by Jakob Hjortshøj on 18/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JP Apex Predators/JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Jakob Hjortshøj on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
